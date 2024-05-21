cb20deb13a565a1df4002b2855339eb1216391e3
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-05-21 13:00:26 +0300

    Update list_comprehension.py

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return[ number for number in num_list if number % 2 == 0]
     pass
 
 def make_exclamation(sentence_list):
+    return[f"{sentence}!" for sentence in sentence_list]
     pass
